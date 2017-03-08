b4d5296e49f1deb3da60a6ea102cb6fdff2aae55
Britt <you@example.com>
2017-03-08 15:12:43 -0500

    Added CreatePlayer & Map method to PlayerController

--- BasketballScoreBook/Controllers/PlayerController.cs
+++ BasketballScoreBook/Controllers/PlayerController.cs
@@ -19,19 +19,39 @@ namespace BasketballScoreBook.Controllers
         }
 
         [HttpGet]
-        public ActionResult InputPlayerInfo()
+        public ActionResult CreatePlayer()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult InputPlayerInfo(PlayerViewModel playerVM)
+        public ActionResult CreatePlayer(PlayerViewModel playerVM)
+        {
+            LogicPlayer boPlayer = Map(playerVM);
+            _playerBLL.CreatePlayer(boPlayer);
+            
+            return View();
+        }
+
+        static LogicPlayer Map(PlayerViewModel playerVM)  //Maps userVM to boUser
         {
             LogicPlayer boPlayer = new LogicPlayer();
 
+            var type_playerVM = playerVM.SinglePlayer.GetType();
+            var type_boPlayer = boPlayer.GetType();
 
+            foreach (var field_playerVM in type_playerVM.GetFields())
+            {
+                var field_boPlayer = type_boPlayer.GetField(field_playerVM.Name);
+                field_boPlayer.SetValue(boPlayer, field_playerVM.GetValue(playerVM.SinglePlayer));
+            }
 
-            return 
+            foreach (var prop_playerVM in type_playerVM.GetProperties())
+            {
+                var prop_boPlayer = type_boPlayer.GetProperty(prop_playerVM.Name);
+                prop_boPlayer.SetValue(boPlayer, prop_playerVM.GetValue(playerVM.SinglePlayer));
+            }
+            return boPlayer;
         }
 
 
