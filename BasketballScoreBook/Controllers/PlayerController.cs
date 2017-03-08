67970a963d8448c861ff640491aa196cc1f6632a
Britt <you@example.com>
2017-03-08 14:58:21 -0500

    Created PlayerController

---
+++ BasketballScoreBook/Controllers/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BasketballScoreBook.Models;
+using BusinessLogicLayer;
+using BusinessLogicLayer.BusinessObjects;
+
+namespace BasketballScoreBook.Controllers
+{
+    public class PlayerController : Controller
+    {
+        static PlayerBusinessLogic _playerBLL = new PlayerBusinessLogic();
+        // GET: Player
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult InputPlayerInfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InputPlayerInfo(PlayerViewModel playerVM)
+        {
+            LogicPlayer boPlayer = new LogicPlayer();
+
+
+
+            return 
+        }
+
+
+    }
+}
--- BusinessLogicLayer/PlayerBusinessLogic.cs
+++ BusinessLogicLayer/PlayerBusinessLogic.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccessLayer;
+using DataAccessLayer.DataAccessObjects;
+using BusinessLogicLayer.BusinessObjects;
 
 namespace BusinessLogicLayer
 {
-    class PlayerBusinessLogic
+    
+    public class PlayerBusinessLogic
     {
+        static PlayerDataAccess _playerDAL = new PlayerDataAccess();
+
     }
 }
