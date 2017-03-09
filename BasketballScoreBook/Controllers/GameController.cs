a937c94311d2c61f316e96be86eb1fb1631bf3b0
Britt <you@example.com>
2017-03-09 16:41:01 -0500

    CerateTeam and Map method in team Controller

---
+++ BasketballScoreBook/Controllers/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BasketballScoreBook.Models;
+using BusinessLogicLayer;
+using BusinessLogicLayer.BusinessObjects;
+
+namespace BasketballScoreBook.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BasketballScoreBook/Controllers/TeamController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BasketballScoreBook.Models;
+using BusinessLogicLayer;
+using BusinessLogicLayer.BusinessObjects;
+
+namespace BasketballScoreBook.Controllers
+{
+    public class TeamController : Controller
+    {
+        static TeamBusinessLogic _teamBLL = new TeamBusinessLogic();
+        // GET: Team
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateTeam()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateTeam(TeamViewModel teamVM)
+        {
+            LogicTeam boTeam = Map(teamVM);
+            _teamBLL.CreateTeam(boTeam);
+            return View();
+        }
+
+        static LogicTeam Map(TeamViewModel teamVM)
+        {
+            LogicTeam boTeam = new LogicTeam();
+
+            var type_teamVM = teamVM.SingleTeam.GetType();
+            var type_boTeam = boTeam.GetType();
+
+            foreach(var field_teamVM in type_teamVM.GetFields())
+            {
+                var field_boTeam = type_boTeam.GetField(field_teamVM.Name);
+                field_boTeam.SetValue(boTeam, field_teamVM.GetValue(teamVM.SingleTeam));
+            }
+
+            foreach (var prop_teamVM in type_teamVM.GetProperties())
+            {
+                var prop_boTeam = type_boTeam.GetProperty(prop_teamVM.Name);
+                prop_boTeam.SetValue(boTeam, prop_teamVM.GetValue(teamVM.SingleTeam));
+            }
+
+            return boTeam;
+        }
+
+    }
+}
