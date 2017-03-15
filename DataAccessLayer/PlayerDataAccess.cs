a5fa35ca4a109dcaa5b779c3d3c2ed73235275a5
Britt <you@example.com>
2017-03-15 16:42:18 -0400

    PlayerDAL ReadPlayers method

--- DataAccessLayer/PlayerDataAccess.cs
+++ DataAccessLayer/PlayerDataAccess.cs
@@ -73,19 +73,42 @@ namespace DataAccessLayer
                     {
                         DataPlayer daReadPlayer = new DataPlayer();
                         daReadPlayer.PlayerID = rdr.GetInt32(rdr.GetOrdinal("PlayerID"));
-                        daReadPlayer.
+                        daReadPlayer.TeamID = rdr.GetInt32(rdr.GetOrdinal("TeamID"));
+                        daReadPlayer.PlayerLastName = (string)rdr["PlayerLastName"];
+                        daReadPlayer.PlayerFirstInitial = (string)rdr["PlayerFirstInitial"];
+                        daReadPlayer.PlayerRole = (string)rdr["PlayerRole"];
+                        daReadPlayer.JerseyNum = rdr.GetInt32(rdr.GetOrdinal("PlayerJerseyNum"));
+                        daReadPlayer.PlayerAssists = rdr.GetInt32(rdr.GetOrdinal("PlayerAssists"));
+                        daReadPlayer.PlayerFouls = rdr.GetInt32(rdr.GetOrdinal("PlayerFouls"));
+                        daReadPlayer.PlayerPoints = rdr.GetInt32(rdr.GetOrdinal("PlayerPoints"));
+                        daReadPlayer.QuarterPlayed = rdr.GetInt32(rdr.GetOrdinal("PlayerQuartersPlayed"));
+                        daReadPlayer.PlayerShotAttempts = rdr.GetInt32(rdr.GetOrdinal("PlayerShotAttempts"));
+                        daReadPlayer.PlayerShotMakes = rdr.GetInt32(rdr.GetOrdinal("PlayerShotMakes"));
 
-
-
+                        ldaPlayerList.Add(daReadPlayer);
+                        
                     }
-
-
-
+                    catch (SqlException error)
+            {
+                using (StreamWriter lWriter = new StreamWriter(_FileLocation, true))
+                {
+                    lWriter.WriteLine(error.Message);
                 }
-
-
-
             }
+            finally
+            {
+                lConnection.Close();
+            }
+            return ldaPlayerList;
+        }
+
+
+
+    }
+
+
+
+}
 
 
 
